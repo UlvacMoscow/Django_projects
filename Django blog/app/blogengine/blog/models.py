e7522108c49683beb8970f99a2e03bd88023bf1a
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-10-03 16:26:19 +0300

    Обновил базу данных для таблицы Post

--- Django blog/app/blogengine/blog/models.py
+++ Django blog/app/blogengine/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.Charfield(max_length=150, db_index=True)
+    title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
