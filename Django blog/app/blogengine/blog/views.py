c9ab030651849e578ba398aa6f88eb540a903fde
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-10-05 22:36:24 +0300

    почистил код

--- Django blog/app/blogengine/blog/views.py
+++ Django blog/app/blogengine/blog/views.py
@@ -5,37 +5,24 @@ from django.shortcuts import get_object_or_404
 
 from .models import Post, Tag
 from .utils import ObjectdetailMixin
-# Create your views here.
-def posts_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts' : posts})
+
 
 
 class PostDetail(ObjectdetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
-    # def get(self, request, slug):
-    #     post = get_object_or_404(Post, slug__iexact=slug)
-    #     return render(request, 'blog/post_detail.html', context={'post': post})
-
-
-# def post_detail(request, slug):
-#     post = Post.objects.get(slug__iexact=slug)
-#     return render(request, 'blog/post_detail.html', context={'post': post})
 
 
 class TagDetail(ObjectdetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
-    # def get(self, request, slug):
-    #         tag = get_object_or_404(Tag, slug__iexact=slug)
-    #         return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
-# def tag_detail(request, slug):
-#     tag = Tag.objects.get(slug__iexact=slug)
-#     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts' : posts})
