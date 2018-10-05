814b9a381a0349784240d05d4da358791ce87025
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-10-05 22:16:59 +0300

    переписали функции-обработчики запросов(views) в классы - Class Based Views

--- Django blog/app/blogengine/blog/views.py
+++ Django blog/app/blogengine/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View
 
 from .models import Post, Tag
 # Create your views here.
@@ -7,9 +8,23 @@ def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts' : posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+
+class PostDetail(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+# def post_detail(request, slug):
+#     post = Post.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+class TagDetail(View):
+    def get(self, request, slug):
+            tag = Tag.objects.get(slug__iexact=slug)
+            return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
 
 def tags_list(request):
     tags = Tag.objects.all()
