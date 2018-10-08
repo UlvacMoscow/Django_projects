d6c940940098696eb058e83690ba82624048b2b9
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-10-08 22:51:46 +0300

    TagForm создали метод который приводит slug к нижнему регистру, и создали проверку на недопустимость slug == 'create'

--- Django blog/app/blogengine/blog/forms.py
+++ Django blog/app/blogengine/blog/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 from .models import Tag
-
+from django.core.exceptions import ValidationError
 
 class TagForm(forms.Form):
     title = forms.CharField(max_length=50)
     slug = forms.CharField(max_length=50)
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug way not be "create"')
+        return new_slug
 
     def save(self):
         new_tag = Tag.objects.create(
