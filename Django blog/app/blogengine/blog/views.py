33ca76a1b05cbf0ce02069b79dba2e3978388b9c
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-10-17 23:36:09 +0300

    добавил логику к клавишам previous, next

--- Django blog/app/blogengine/blog/views.py
+++ Django blog/app/blogengine/blog/views.py
@@ -75,4 +75,23 @@ def posts_list(request):
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
 
-    return render(request, 'blog/index.html', context={'page_object' : page})
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+        'page_object' : page,
+        'is_paginated' : is_paginated,
+        'next_url' : next_url,
+        'prev_url' : prev_url
+    }
+
+    return render(request, 'blog/index.html', context=context)
