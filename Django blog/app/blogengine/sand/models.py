6c9ebd38903f45a6fa5b839fcca7a1edd7cbb0c6
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-05-28 22:30:12 +0300

    add two models with related_name

--- Django blog/app/blogengine/sand/models.py
+++ Django blog/app/blogengine/sand/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=128)
+
+    # по related_name можно получить связанную модель в любую сторону
+    def get_groups(self):
+        return self.groups.all()
+
+    def get_groups_related_set():
+        # user.group_set.all() если related name не задан
+        pass
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(User, related_name='groups')
