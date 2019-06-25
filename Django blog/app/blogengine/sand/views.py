7cd7e42b3bfe3b4115018bc68b2180d7e25a4ee0
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-06-25 22:57:24 +0300

    add ListView for zombie, vampire

--- Django blog/app/blogengine/sand/views.py
+++ Django blog/app/blogengine/sand/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-from .models import Ghost
-from django.views import generic
+from .models import Ghost, Vampire, Zombie
+from django.views.generic import ListView, TemplateView
 from django.shortcuts import render
 
 # Create your views here.
 
-class GhostListView(generic.ListView):
+class GhostListView(ListView):
     model = Ghost
     template = 'ghost_list.html'
+    queryset = Ghost.objects.all()
+
+
+class ZombieListView(ListView):
+    model = Zombie
+    template = 'zombie_list.html'
+    queryset = Zombie.objects.all()
+
+
+class VampireListView(ListView):
+    model = Vampire
+    template = 'vampire_list.html'
+    queryset = Vampire.objects.all()
 
 
 def show_elem(request):
