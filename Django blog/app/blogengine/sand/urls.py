c7fcac789c3f03689979590caa41853aaf4e73d1
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-06-25 22:59:47 +0300

    add urls for ListView

--- Django blog/app/blogengine/sand/urls.py
+++ Django blog/app/blogengine/sand/urls.py
@@ -5,5 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', GhostListView.as_view(), name='ghosts_list_url'),
+    path('vampires/', VampireListView.as_view(), name='vampire_list_url'),
+    path('zombies/', ZombieListView.as_view(), name='zombie_list_url'),
     path('elements/', show_elem, name='show_elem_url')
 ]
